c8ab7c7d935c42bc12ad47cfe2ef8069d1ed1fd1
Javier Antoran Cabiscol <javir803@gmail.com>
2018-05-16 21:43:56 +0200

    updated architecture

--- NN_solution/delta_spectrogram_simplenet/simplenet.py
+++ NN_solution/delta_spectrogram_simplenet/simplenet.py
@@ -36,10 +36,6 @@ class simplenet(nn.Module):
                              nn.BatchNorm2d(128, eps=1e-05, momentum=0.05, affine=True),
                              nn.ReLU(inplace=True),
 
-                             nn.Conv2d(128, 128, kernel_size=[3, 3], stride=(1, 1), padding=(1, 1)),
-                             nn.BatchNorm2d(128, eps=1e-05, momentum=0.05, affine=True),
-                             nn.ReLU(inplace=True),
-
                              #
                              nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2), dilation=(1, 1), ceil_mode=False),
                              nn.Dropout2d(p=0.1),
@@ -49,9 +45,6 @@ class simplenet(nn.Module):
                              nn.BatchNorm2d(128, eps=1e-05, momentum=0.05, affine=True),
                              nn.ReLU(inplace=True),
 
-                             nn.Conv2d(128, 128, kernel_size=[3, 3], stride=(1, 1), padding=(1, 1)),
-                             nn.BatchNorm2d(128, eps=1e-05, momentum=0.05, affine=True),
-                             nn.ReLU(inplace=True),
 
                              nn.Conv2d(128, 256, kernel_size=[3, 3], stride=(1, 1), padding=(1, 1)),
                              nn.BatchNorm2d(256, eps=1e-05, momentum=0.05, affine=True),
@@ -79,8 +72,8 @@ class simplenet(nn.Module):
 
 
 
-                             nn.Conv2d(256, 512, kernel_size=[3, 3], stride=(1, 1), padding=(1, 1)),
-                             nn.BatchNorm2d(512, eps=1e-05, momentum=0.05, affine=True),
+                             nn.Conv2d(256, 256, kernel_size=[3, 3], stride=(1, 1), padding=(1, 1)),
+                             nn.BatchNorm2d(256, eps=1e-05, momentum=0.05, affine=True),
                              nn.ReLU(inplace=True),
 
 
@@ -89,13 +82,7 @@ class simplenet(nn.Module):
                              nn.Dropout2d(p=0.1),
 
 
-                             nn.Conv2d(512, 2048, kernel_size=[1, 1], stride=(1, 1), padding=(0, 0)),
-                             nn.BatchNorm2d(2048, eps=1e-05, momentum=0.05, affine=True),
-                             nn.ReLU(inplace=True),
-
-
-
-                             nn.Conv2d(2048, 256, kernel_size=[1, 1], stride=(1, 1), padding=(0, 0)),
+                             nn.Conv2d(256, 256, kernel_size=[3, 3], stride=(1, 1), padding=(1, 1)),
                              nn.BatchNorm2d(256, eps=1e-05, momentum=0.05, affine=True),
                              nn.ReLU(inplace=True),
 
