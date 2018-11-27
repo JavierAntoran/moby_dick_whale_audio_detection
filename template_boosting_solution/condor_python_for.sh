24c2ab4666deebe789068ffcac56256c15a54123
Alberto Mur Lopez <565825@unizar.es>
2018-11-27 02:17:23 +0100

    Condor loop script

---
+++ template_boosting_solution/condor_python_for.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+file=$1
+n_jobs=$2
+
+#-------------------------------------------------------------------
+# create dir
+mkdir -p .condor/
+
+#-------------------------------------------------------------------
+# base filename
+
+filename_base="${file%.*}"
+
+#-------------------------------------------------------------------
+# create condor config
+
+condor_task=.condor/${filename_base}.sub
+
+#::::::::::::::::::::::::::::::::::::::::::
+cat > ${condor_task} << EOF
+
+executable = `which python`
+arguments  = ${file} \$(icluster)
+output     = ${filename_base}_\$(ifile).log
+error      = ${filename_base}_\$(ifile).err
+log        = ${filename_base}.clog
+
+EOF
+#::::::::::::::::::::::::::::::::::::::::::
+
+for((i=1;i<=$n_jobs;i++)); do
+	  ifile=`echo $i | awk '{ printf("%03d",$1);}'`
+      echo "
+      ifile = $ifile
+      icluster = $i
+      queue " 
+done >> ${condor_task}
+
+#-------------------------------------------------------------------
+# view the condor config
+
+#cat ${condor_task}
+
+#-------------------------------------------------------------------
+# launch condor job
+
+condor_submit.sh --sync y --config ${condor_task} 
+
+
+#-------------------------------------------------------------------
+# clean
+
+#rm -rf .condor
